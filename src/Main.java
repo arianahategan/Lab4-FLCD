63e5fe0d15bcf5788c795caa0eb7edb853ad7855
Ariana Hategan <arianahategan@yahoo.com>
2020-11-04 10:58:53 +0200

    Created FA

---
+++ src/Main.java
@@ -0,0 +1,149 @@
+import finiteAutomata.FiniteAutomaton;
+import finiteAutomata.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> states = new ArrayList<String>(Arrays.asList(readStatesFromFile()));
+        ArrayList<String> alphabet = new ArrayList<String>(Arrays.asList(readAlphabetFromFile()));
+        ArrayList<String> initialStates = new ArrayList<String>(Arrays.asList(readInitialStatesFromFile()));
+        ArrayList<String> finalStates = new ArrayList<String>(Arrays.asList(readStatesFromFile()));
+        HashMap<Pair, ArrayList<String>> transitions = readTransitionsFromFile();
+
+
+        System.out.println("States: " + states);
+        System.out.println("Alphabet: " + alphabet);
+        System.out.println("Initial States: " + initialStates);
+        System.out.println("Final States: " + finalStates);
+        System.out.println("Transitions: " + transitions);
+
+        FiniteAutomaton finiteAutomaton = new FiniteAutomaton(states, alphabet, transitions, initialStates, finalStates);
+
+        if(finiteAutomaton.isDeterministic())
+            System.out.println("FA is deterministic");
+        else
+            System.out.println("FA is not deterministic");
+
+    }
+
+    private static String[] readStatesFromFile() {
+        String[] states = new String[0];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fa.in"));
+            String line = reader.readLine();
+            int lineIndex = 0;
+            while(line != null){
+                if(lineIndex == 0)
+                    states = line.split(":")[1].split(",");
+                line = reader.readLine();
+                lineIndex++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return states;
+    }
+
+    private static String[] readAlphabetFromFile() {
+        String[] alphabet = new String[0];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fa.in"));
+            String line = reader.readLine();
+            int lineIndex = 0;
+            while(line != null){
+                if(lineIndex == 1)
+                    alphabet = line.split(":")[1].split(",");
+                line = reader.readLine();
+                lineIndex++;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return alphabet;
+    }
+
+    private static String[] readInitialStatesFromFile() {
+        String[] initialStates = new String[0];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fa.in"));
+            String line = reader.readLine();
+            int lineIndex = 0;
+            while(line != null){
+                if(lineIndex == 2)
+                    initialStates = line.split(":")[1].split(",");
+                line = reader.readLine();
+                lineIndex++;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return initialStates;
+    }
+
+    private static String[] readFinalStatesFromFile() {
+        String[] finalStates = new String[0];
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fa.in"));
+            String line = reader.readLine();
+            int lineIndex = 0;
+            while(line != null){
+                if(lineIndex == 4)
+                    finalStates = line.split(":")[1].split(",");
+                line = reader.readLine();
+                lineIndex++;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return finalStates;
+    }
+
+    private static HashMap<Pair, ArrayList<String>> readTransitionsFromFile() {
+        HashMap<Pair, ArrayList<String>> transitions = new HashMap<>();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("fa.in"));
+            String line = reader.readLine();
+            int lineIndex = 0;
+            while(line != null){
+                if(lineIndex == 3)
+                {
+                    String[] transitionStrings = line.split(":")[1].split(";");
+                    for(String transitionString: transitionStrings) {
+                        String[] elements = transitionString.split("-");
+                        Pair pair = new Pair(elements[0], elements[1]);
+                        String[] aux = elements[2].split(",");
+                        ArrayList<String> states = new ArrayList<String>(Arrays.asList(aux));
+
+                        transitions.put(pair, states);
+                    }
+                }
+                line = reader.readLine();
+                lineIndex++;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return transitions;
+    }
+}
---
+++ src/finiteAutomata/FiniteAutomaton.java
@@ -0,0 +1,81 @@
+package finiteAutomata;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FiniteAutomaton {
+    private ArrayList<String> states;
+    private ArrayList<String> alphabet;
+    private HashMap<Pair, ArrayList<String>> transitions;
+    private ArrayList<String> initialState;
+    private ArrayList<String> finalStates;
+
+    public FiniteAutomaton(ArrayList<String> states, ArrayList<String> alphabet, HashMap<Pair, ArrayList<String>> transitions, ArrayList<String> initialState, ArrayList<String> finalStates) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.transitions = transitions;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+    }
+
+    public ArrayList<String> getStates() {
+        return states;
+    }
+
+    public void setStates(ArrayList<String> states) {
+        this.states = states;
+    }
+
+    public ArrayList<String> getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(ArrayList<String> alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public HashMap<Pair, ArrayList<String>> getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(HashMap<Pair, ArrayList<String>> transitions) {
+        this.transitions = transitions;
+    }
+
+    public ArrayList<String> getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(ArrayList<String> initialState) {
+        this.initialState = initialState;
+    }
+
+    public ArrayList<String> getFinalStates() {
+        return finalStates;
+    }
+
+    public void setFinalStates(ArrayList<String> finalStates) {
+        this.finalStates = finalStates;
+    }
+
+    public boolean isDeterministic() {
+        for(Map.Entry<Pair, ArrayList<String>> e : transitions.entrySet()){
+            if(e.getValue().size() > 1)
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "FiniteAutomaton{" +
+                "states=" + states +
+                ", alphabet=" + alphabet +
+                ", transitions=" + transitions +
+                ", initialState='" + initialState + '\'' +
+                ", finalStates=" + finalStates +
+                '}';
+    }
+}
